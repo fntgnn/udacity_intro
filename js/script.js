9ee212940939e0fa9374b2deab93341ba7cd78ad
ftngnn <gio.fon@gmail.com>
2017-05-24 11:50:59 +0200

    simple, just titles

--- js/script.js
+++ js/script.js
@@ -13,10 +13,17 @@ function loadData() {
 
     // load streetview
     var address = $('#street').val() + ',' +  $('#city').val();
-    //var img = ;
     $body.append('<img class="bgimg" src="http://maps.googleapis.com/maps/api/streetview?size=600x300&location='+address+'">');
 
     // YOUR CODE GOES HERE!
+
+    $.getJSON("https://api.nytimes.com/svc/search/v2/articlesearch.json?q="+address+"&sort=newest&api-key=914217aed4144766b7c910e094ce1e95", function(result){
+      const docs = result.response.docs;
+      for(var i=0; i<docs.length; i++){
+        $nytElem.append('<li class="article">'+docs[i].headline.main+'</li>');
+      }
+      console.log(result.response.docs);
+    })
 
     return false;
 };
